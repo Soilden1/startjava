20b5ed6fd2d4ad1e259c969e42f739c950e9ad07
Soilden1 <130134782+Soilden1@users.noreply.github.com>
2023-04-12 21:51:52 +0300

    Изменил вывод текста, отображаемого в консоль

--- Lesson_1/MyFirstApp.java
+++ Lesson_1/MyFirstApp.java
@@ -1,6 +1,7 @@
 public class MyFirstApp {
-
+    
     public static void main(String[] args) {
-        System.out.print("Написано однажды, работает везде!");
+        System.out.print("Написано однажды, ");
+        System.out.println("работает везде!");
     }
 }
