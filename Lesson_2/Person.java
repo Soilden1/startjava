dcefb0a505ca1db9ff8a83c50c719aee050dae43
Dmitry_Smirnov <dimacm14@gmail.com>
2023-04-20 10:39:46 +0300

    Implement Person

---
+++ Lesson_2/Person.java
@@ -0,0 +1,29 @@
+public class Person {
+
+    String gender = "male";
+    String name = "Ivan";
+    int height = 180;
+    int weight = 72;
+    int age = 18;
+
+    void move() {
+        System.out.println("Человек идет");
+    }
+
+    void sit() {
+        System.out.println("Человек сидит");
+    }
+
+    void run() {
+        System.out.println("Человек бежит");
+    }
+
+    void talk() {
+        System.out.println("Человек говорит");
+    }
+
+    void learnJava() {
+        System.out.println("Человек учит Java");
+    }
+}
+
