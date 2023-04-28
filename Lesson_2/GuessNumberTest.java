b22cd2e8cd9fac0e588eb7c479db9ff5b52eeeb7
Dmitry_Smirnov <dimacm14@gmail.com>
2023-04-28 18:26:01 +0300

    Corrected GuessNumberTest

--- Lesson_2/GuessNumberTest.java
+++ Lesson_2/GuessNumberTest.java
@@ -5,13 +5,13 @@ class GuessNumberTest {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String continuation = "yes";
-
-        while (continuation.equals("yes")) {
-            System.out.print("Введите имя первого игрока: ");
+        
+        System.out.print("Введите имя первого игрока: ");
             Player player1 = new Player(sc.next());
             System.out.print("Введите имя второго игрока: ");
             Player player2 = new Player(sc.next());
 
+        while (continuation.equals("yes")) {
             GuessNumber gn = new GuessNumber(player1, player2);
             gn.startGuess();
 
